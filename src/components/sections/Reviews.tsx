73bcd3b82345ed578500688b38d1990b1113d210
SolCaruso <caruso@ocelot.ltd>
2025-05-04 00:41:42 -0400

    adds reviews

--- src/components/sections/Reviews.tsx
+++ src/components/sections/Reviews.tsx
@@ -15,87 +15,87 @@ interface Review {
 
 const reviews: Array<Review> = [
   {
-    title: 'It really works.',
-    body: 'I downloaded Pocket today and turned $5000 into $25,000 in half an hour.',
-    author: 'CrazyInvestor',
+    title: 'Everything so far is great.',
+    body: 'Played everything out in Early Access, everything so far is great. Interesting story, fun quests, challenging but rewarding combat. Excited for more content.',
+    author: 'stringy hutch',
     rating: 5,
   },
   {
-    title: 'You need this app.',
-    body: 'I didn’t understand the stock market at all before Pocket. I still don’t, but at least I’m rich now.',
-    author: 'CluelessButRich',
+    title: 'Divinity Original Sin vibes.',
+    body: 'Very nice cRPG game, with Divinity Original Sin vibes.',
+    author: 'Yuumi',
     rating: 5,
   },
   {
-    title: 'This shouldn’t be legal.',
-    body: 'Pocket makes it so easy to win big in the stock market that I can’t believe it’s actually legal.',
-    author: 'LivingDaDream',
+    title: 'The graphics are stunning.',
+    body: 'An absolute blast to play. The graphics are stunning, the story is engaging and keeps you hooked from start to finish, with just the right amount of twists.',
+    author: 'MikiAiko',
     rating: 5,
   },
   {
-    title: 'Screw financial advisors.',
-    body: 'I barely made any money investing in mutual funds. With Pocket, I’m doubling my net-worth every single month.',
-    author: 'JordanBelfort1962',
+    title: "One of the greatest indie RPG's on Steam.",
+    body: "Only a few hours in, and I can safely say this is one of the greatest indie RPG's to ever grace the Steam Store Page.",
+    author: 'Veepee',
     rating: 5,
   },
   {
-    title: 'I love it!',
-    body: 'I started providing insider information myself and now I get new insider tips every 5 minutes. I don’t even have time to act on all of them. New Lamborghini is being delivered next week!',
-    author: 'MrBurns',
+    title: 'Incredibly detailed and customizable RPG with beautiful art and music.',
+    body: 'An incredibly detailed and customizable RPG with beautiful art and music. The game has tons of potential to become part of a great saga.',
+    author: 'Vanhelsing',
     rating: 5,
   },
   {
-    title: 'Too good to be true.',
-    body: 'I was making money so fast with Pocket that it felt like a scam. But I sold my shares and withdrew the money and it’s really there, right in my bank account. This app is crazy!',
-    author: 'LazyRich99',
+    title: 'I want more.',
+    body: 'It manages to scratch that Baldurs Gate itch as well as that FF Tactics itch at the same time. I want more.',
+    author: 'Demonic Enterprise',
     rating: 5,
   },
   {
-    title: 'Wish I could give 6 stars',
-    body: 'This is literally the most important app you will ever download in your life. Get on this before it’s so popular that everyone else is getting these tips too.',
-    author: 'SarahLuvzCash',
+    title: "Can't wait to play the full game",
+    body: "Gameplay is like BG3 but with great old school inspired pixel art. Can't wait to play the full game.",
+    author: 'Robbie Dagger',
     rating: 5,
   },
   {
-    title: 'Bought an island.',
-    body: 'Yeah, you read that right. Want your own island too? Get Pocket.',
-    author: 'ScroogeMcduck',
+    title: 'I love the art style.',
+    body: 'Really cool game and I love the art style. Gives ultima vibes.',
+    author: '똑똑한',
     rating: 5,
   },
   {
-    title: 'No more debt!',
-    body: 'After 2 weeks of trading on Pocket I was debt-free. Why did I even go to school at all when Pocket exists?',
-    author: 'BruceWayne',
+    title: 'A Promising Early Access RPG.',
+    body: "An RPG that shows immense promise, even in its Early Access stage. I’ve been thoroughly enjoying my time with it so far and it deserves a solid 5 stars.",
+    author: 'DontThatsMyBum',
     rating: 5,
   },
   {
-    title: 'I’m 13 and I’m rich.',
-    body: 'I love that with Pocket’s transaction anonymization I could sign up and start trading when I was 12 years old. I had a million dollars before I had armpit hair!',
-    author: 'RichieRich',
+    title: 'Divinity meets Final Fantasy Tactics.',
+    body: "It's basically Divinity: Original Sin 2 meets Final Fantasy Tactics.",
+    author: 'Rhuzkii',
     rating: 5,
   },
   {
-    title: 'Started an investment firm.',
-    body: 'I charge clients a 3% management fee and just throw all their investments into Pocket. Easy money!',
-    author: 'TheCountOfMonteChristo',
+    title: 'Very few games give me the feel this game does.',
+    body: 'Very few games give me the feel this game does, and it has been years since I last encountered it.',
+    author: 'Rhuvian',
     rating: 5,
   },
   {
-    title: 'It’s like a superpower.',
-    body: 'Every tip Pocket has sent me has paid off. It’s like playing Blackjack but knowing exactly what card is coming next!',
-    author: 'ClarkKent',
+    title: 'Combat is addicting.',
+    body: 'This game is pretty awesome. Combat is addicting and quests do a good job of engaging you.',
+    author: 'DivineBlood',
     rating: 5,
   },
   {
-    title: 'Quit my job.',
-    body: 'I downloaded Pocket three days ago and quit my job today. I can’t believe no one else thought to build a stock trading app that works this way!',
-    author: 'GeorgeCostanza',
+    title: 'The CRPG legend endorses this game.',
+    body: 'Insane potential, deserves support, responsive dev, fun character and class variety, valid to wait for full release. The CRPG legend endorses this game.',
+    author: 'KingAthena',
     rating: 5,
   },
   {
-    title: 'Don’t download this app',
-    body: 'Unless you want to have the best life ever! I am literally writing this from a yacht.',
-    author: 'JeffBezos',
+    title: 'Definitely recommend.',
+    body: 'Definitely recommend, everything works very smoothly , combat is actually fun and can be difficult, not the same SUPER predictable combat bs.',
+    author: 'The Ninja',
     rating: 5,
   },
 ]
@@ -103,7 +103,14 @@ const reviews: Array<Review> = [
 function StarIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
-      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+      <defs>
+        <linearGradient id="starGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" stopColor="#FBAA6D" />
+          <stop offset="50%" stopColor="#F8F289" />
+          <stop offset="100%" stopColor="#FC8A52" />
+        </linearGradient>
+      </defs>
+      <path fill="url(#starGradient)" d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
     </svg>
   )
 }
@@ -114,10 +121,8 @@ function StarRating({ rating }: { rating: Review['rating'] }) {
       {[...Array(5).keys()].map((index) => (
         <StarIcon
           key={index}
-          className={clsx(
-            'h-5 w-5',
-            rating > index ? 'fill-[#f4c016]' : 'fill-stone-400',
-          )}
+          className={rating > index ? 'h-5 w-5' : 'h-5 w-5 text-stone-400'}
+          fill={rating > index ? 'url(#starGradient)' : undefined}
         />
       ))}
     </div>
@@ -142,7 +147,7 @@ function Review({
   return (
     <figure
       className={clsx(
-        'animate-fade-in rounded-lg bg-[#141414] p-6 opacity-0 border border-[#222222]',
+        'animate-fade-in rounded-lg bg-[#101010] p-6 opacity-0 border border-[#222222]',
         className,
       )}
       style={{ animationDelay }}
@@ -280,12 +285,12 @@ export function Reviews() {
       <Container>
         <h2
           id="reviews-title"
-          className="text-3xl font-medium tracking-tight text-stone-50 sm:text-center"
+          className="text-3xl font-medium tracking-tight text-stone-100 text-center font-oldFenris uppercase text-pretty mt-8"
         >
-          Everyone is changing their life with Pocket.
+          What Guildies are saying
         </h2>
-        <p className="mt-2 text-lg text-stone-400 sm:text-center">
-          Thousands of people have doubled their net-worth in the last 30 days.
+        <p className="mt-6 sm:mt-3 text-xl text-stone-400 text-center font-quattrocento text-pretty max-w-xl mx-auto">
+          Real reviews from adventurers playing Guild Saga: Vanished Worlds Early Access on Steam.
         </p>
         <ReviewGrid />
       </Container>
